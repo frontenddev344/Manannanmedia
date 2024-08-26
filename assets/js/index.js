da651a8164f21121eec77fd4effc854d96492fa1
Dev avology <devavology12@gmail.com>
2024-08-26 11:01:05 +0530

    done

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+// Toggle Menu Functionaliy Start
+
+$(document).ready(function () {
+    $(".menu-icon").click(function () {
+      $("body").addClass("menuToggle");
+    });
+    $(".close-icon").click(function () {
+      $("body").removeClass("menuToggle");
+    });
+  });
+  
+  // Toggle Menu Functionaliy End
+
+   // Header Scroll JS Start
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var header = $("header");
+    header.toggleClass("fixed-header", $(window).scrollTop() > 0);
+  });
+});
+// Header Scroll JS End
+
+
+
+// AOS Js Start
+
+AOS.init();
+
+// AOS Js End
+
+ //Cookies JS Start
+const cookiesBox = document.querySelector('.wrapper'),
+  buttons = document.querySelectorAll('.button');
+
+// ---- ---- Show ---- ---- //
+const executeCodes = () => {
+  if (document.cookie.includes('AlexGolovanov')) return;
+  cookiesBox.classList.add('show');
+
+  // ---- ---- Button ---- ---- //
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      cookiesBox.classList.remove('show');
+
+      // ---- ---- Time ---- ---- //
+      if (button.id == 'acceptBtn') {
+        document.cookie =
+          'cookieBy= AlexGolovanov; max-age=' + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+window.addEventListener('load', executeCodes);
+
+// //Cookies JS Start
+
